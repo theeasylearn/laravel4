74c71f23cc6aafc2809ce9fa714d7cae20af62a6
theeasylearn <theeasylearn@users.noreply.github.com>
2020-10-22 10:08:54 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\quote;
+use Facade\FlareClient\Http\Response;
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -10,6 +12,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get("/quotes",function(){
     return quote::all();
 });
+
 //retrive specific quote
 Route::get("/quotes/{id}",function(Request $request,$id){
     $response = quote::find($id);
@@ -28,3 +31,11 @@ Route::delete("/quotes/{id}",function($id){
     return quote::destroy($id);
 });
 
+//update route 
+Route::post("/quotes_2",function(Request $request)
+{
+    $qt=quote::find($request->id);
+    $qt->fill($request->all());
+    $qt->save();
+    return $qt;
+});
